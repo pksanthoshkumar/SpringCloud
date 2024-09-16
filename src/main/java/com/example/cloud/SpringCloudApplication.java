537164357732c6663a457580b6b9c7c4625556e1
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-09-16 11:52:39 -0400

    Spring Cloud (Eureka) Client

--- src/main/java/com/example/cloud/SpringCloudApplication.java
+++ src/main/java/com/example/cloud/SpringCloudApplication.java
@@ -2,10 +2,9 @@ package com.example.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
-@EnableEurekaServer
 public class SpringCloudApplication {
 
 	public static void main(String[] args) {
