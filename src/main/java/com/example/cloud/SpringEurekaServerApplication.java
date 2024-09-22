2bd5587aa061e996eef1849b8ef5135b45e22259
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-09-21 20:48:09 -0400

    Enable Eureka Server

--- src/main/java/com/example/cloud/SpringEurekaServerApplication.java
+++ src/main/java/com/example/cloud/SpringEurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudApplication {
+public class SpringEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudApplication.class, args);
+		SpringApplication.run(SpringEurekaServerApplication.class, args);
 	}
 
 }
